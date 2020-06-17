16136aa0df420d8db1df070a38c9811e68762a11
Yasuaki Uechi <y@uechi.io>
2020-06-18 00:43:36 +0900

    feat(remove): multiple entry

--- command_remove.go
+++ command_remove.go
@@ -13,6 +13,12 @@ import (
 var commandRemove = cli.Command{
 	Name:   "remove",
 	Action: doRemove,
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "f, force",
+			Usage: "Remove without prompt",
+		},
+	},
 }
 
 // IsEmpty checks if directory is empty
@@ -31,39 +37,43 @@ func IsEmpty(name string) (bool, error) {
 }
 
 func doRemove(c *cli.Context) error {
-	target := compileTargetPath(c.Args().Get(0))
+	forceRemove := c.Bool("force")
 
-	if _, err := os.Stat(target); err == nil {
-		if !prompter.YN("Remove? "+target, true) {
-			os.Exit(0)
-		}
+	for _, arg := range c.Args() {
+		target := compileTargetPath(arg)
 
-		// Remove specified directory
-		err := os.RemoveAll(target)
-		if err == nil {
-			fmt.Println("Removed: " + target)
-		} else {
-			fmt.Println(err)
-		}
-
-		// Remove parent dirs if empty
-		ghqPath, _ := getGhqPath()
-		target = filepath.Dir(target)
-		for target != ghqPath {
-			if e, _ := IsEmpty(target); !e {
-				break
+		if _, err := os.Stat(target); err == nil {
+			if !forceRemove && !prompter.YN("Remove? "+target, true) {
+				os.Exit(0)
 			}
+
+			// Remove specified directory
 			err := os.RemoveAll(target)
 			if err == nil {
 				fmt.Println("Removed: " + target)
 			} else {
 				fmt.Println(err)
-				break
 			}
+
+			// Remove parent dirs if empty
+			ghqPath, _ := getGhqPath()
 			target = filepath.Dir(target)
+			for target != ghqPath {
+				if e, _ := IsEmpty(target); !e {
+					break
+				}
+				err := os.RemoveAll(target)
+				if err == nil {
+					fmt.Println("Removed: " + target)
+				} else {
+					fmt.Println(err)
+					break
+				}
+				target = filepath.Dir(target)
+			}
+		} else {
+			fmt.Println("Doesn't exist: " + target)
 		}
-	} else {
-		fmt.Println("Doesn't exist: " + target)
 	}
 
 	return nil
